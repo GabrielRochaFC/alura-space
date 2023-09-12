bec680be300862f083af5be8c09654873047907e
Gabriel Rocha <gabriel.rocha20@live.com>
2023-09-12 12:12:53 -0300

    Medias and filters

--- setup/settings.py
+++ setup/settings.py
@@ -130,6 +130,12 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "setup/static")]
 # Precisamos passar, também, o caminho absoluto para que o diretório consiga pegarr os arquivos estáticos. Para isso, vamos inserir a "raiz" dos caminhos com STATIC_ROOT, com o caminho `os.path.join(BASE_DIR, 'static'):
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+# MEDIA
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
